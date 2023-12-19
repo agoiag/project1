fa3753a4f60a4828241e4bd0e7ccacfb8a91dee5
노현우 <shgusdnzoq@naver.com>
2023-12-19 15:04:10 +0900

    23-12-19

---
+++ hyunwooproject/Assets/monster.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class monster : MonoBehaviour
+{
+    public int id;
+    public int hp;
+    public int atkpower;
+    public int def;
+    public int cri;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
