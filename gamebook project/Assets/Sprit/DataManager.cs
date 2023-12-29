4439fa0dfc66dca9d2c2d8b49909f5bafe40095d
노현우 <shgusdnzoq@naver.com>
2023-12-29 15:16:02 +0900

    23-12-29

--- gamebook project/Assets/Sprit/DataManager.cs
+++ gamebook project/Assets/Sprit/DataManager.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class DataManager : Singleton<DataManager>
 {
     public List<Dictionary<string, object>> talkTable;
-    public List<Dictionary<string, object>> selectTable;
+    public List<Dictionary<string, object>> choisetable;
     public List<Dictionary<string, object>> battleTable;
 
     private void Awake()
     {
-        talkTable = CSVReader.Read("talk_table");
-        selectTable = CSVReader.Read("select_table");
-        battleTable = CSVReader.Read("battle_table");
+        talkTable = CSVReader.Read("talk table");
+        choisetable = CSVReader.Read("choise table");
+        battleTable = CSVReader.Read("battle table");
     }
 }
--- gamebook project/Assets/Sprit/Start_sprit.cs
+++ gamebook project/Assets/Sprit/Start_sprit.cs
@@ -3,15 +3,76 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
+using System;
 
 public class Start_sprit : MonoBehaviour
 {
     public TextMeshProUGUI nameText, descText;
+    public Image chrimage, bgimage;
+    public Button chi1, chi2, chi3;
+    public GameObject chiBtn1, chiBtn2, chiBtn3;
     int descId;
 
     private void Start()
     {
+        chiBtn1.SetActive(false);
+        chiBtn2.SetActive(false);
+        chiBtn3.SetActive(false);
         descId = 0;
-        descText.text = (string)DataManager.Instance.talkTable[descId]["Desc"];
+        //descText.text = (string)DataManager.Instance.talkTable[descId]["Desc"];
+        PlayDesc(descId);
     }
+    private int nextIndex = -1;
+    public void PlayDesc(int descIndex)
+    {
+        string descString = (string)DataManager.Instance.talkTable[descIndex]["Desc"];
+        string nameString = (string)DataManager.Instance.talkTable[descIndex]["Name"];
+        string imageString = (string)DataManager.Instance.talkTable[descIndex]["ChrImage"];
+        string bgString = (string)DataManager.Instance.talkTable[descIndex]["BGImage"];
+        // chrimage.sprite = imageString;
+        chrimage.sprite = Resources.Load<Sprite>(imageString);
+        bgimage.sprite = Resources.Load<Sprite>(bgString);
+        descText.text = descString;
+        nameText.text = nameString;
+        if (nextIndex > 0)
+        {
+            nextIndex = (int)DataManager.Instance.talkTable[descIndex]["Nextid"] - 1;
+        }
+        else if(nextIndex == 0)
+        {
+            string cString = (string)DataManager.Instance.choisetable[descIndex]["Desc"];
+            descText.text = cString;
+        }
+
+
+    }
+    public void OnClickNextDesc()
+    {
+        if(descId == 14)
+        {
+            chiBtn1.SetActive(true);
+            chiBtn2.SetActive(true);
+            chi1.onClick.RemoveAllListeners();
+            chi2.onClick.RemoveAllListeners();
+            chi1.onClick.AddListener(() => OnSelect(15));
+            chi2.onClick.AddListener(() => OnSelect(19));
+            chiBtn1.SetActive(false);
+        } 
+        else if (descId == 36)
+        {
+            chiBtn1.SetActive(true);
+            chi1.onClick.RemoveAllListeners();
+            //chi1.onClick.AddListener(() => OnSelect(37));
+            chiBtn1.SetActive(false);
+        }
+        PlayDesc(nextIndex);
+    }
+
+    private void OnSelect(int v)
+    {
+        throw new NotImplementedException();
+    }
+
+    // void OnSelect(int id) { descId 대사 출력 }
 }
